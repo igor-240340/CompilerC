f890a448642291abd39d7b4927ad3d8f387aa414
igor <igor.240340@gmail.com>
2025-08-31 04:15:45 +0700

    Implement Assembly generator
    In progress.

--- CompilerC/main.cpp
+++ CompilerC/main.cpp
@@ -5,26 +5,28 @@
 
 #include "LexicalAnalyzer.h"
 #include "Parser.h"
+#include "AsmGen.h"
 
 int main() {
-    try {
-        std::string c_program =
-            "int main(void) {"
-            "    return 12;"
-            "}";
+	try {
+		std::string c_program =
+			"int main(void) {"
+			"    return 12;"
+			"}";
 
-        std::list<LexicalAnalyzer::Token> tokens = LexicalAnalyzer::tokenize(c_program);
-        for (const LexicalAnalyzer::Token& token : tokens)
-            std::cout << token.value << ' ';
+		std::list<LexicalAnalyzer::Token> tokens = LexicalAnalyzer::tokenize(c_program);
+		for (const LexicalAnalyzer::Token& token : tokens)
+			std::cout << token.value << ' ';
 
-        std::cout << '\n';
+		std::cout << '\n';
 
-        std::unique_ptr<Parser::Stmt> root = Parser::parse(tokens);
+		std::unique_ptr<Parser::Stmt> c_ast_root = Parser::parse(tokens);
+		std::unique_ptr<AsmGen::Function> asm_ast_root = AsmGen::generate(*c_ast_root);
 
-        return 0;
-    }
-    catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-        return 1;
-    }
+		return 0;
+	}
+	catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
 }
